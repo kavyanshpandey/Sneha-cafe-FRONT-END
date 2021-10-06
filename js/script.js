0d0239970bd8cfc20aedf6cc8005a36b296e5b7f
Maahi <68476173+maahicodes@users.noreply.github.com>
2021-10-06 22:29:10 +0530

    made homepage responsive

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@ toggle.addEventListener('click', () => {
   navigation.classList.toggle('active')
   logoColor.classList.toggle('active')
 })
+
+//dark-light toggle
+function dark() {
+
+  if (document.body.style.backgroundColor == "grey") {
+      document.body.style.backgroundColor = "bisque"
+      document.body.style.Color = "black"
+
+  } else {
+      document.body.style.backgroundColor = "grey";
+      document.body.style.Color = "blue"
+
+  }
+}
