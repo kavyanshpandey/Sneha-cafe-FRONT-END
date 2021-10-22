654e72299839b964948604d034d8e59bcd2da2e4
Hastech77 <maheshwariharshit85@gmail.com>
2021-10-22 19:01:55 +0530

    Reworked the Contact page

--- js/script.js
+++ js/script.js
@@ -24,3 +24,15 @@ function sendToHome() {
     location.href = "../index.html";
 }
 
+
+$( "#contact_form" ).submit(function( event ) {
+    var text = $('#contact_form').find('input[name="name"]').val();
+    $("#contact_form").addClass("shrink");
+    $(".container").addClass("bgchange");
+    $(".thanks").addClass("reveal");
+    $("body").addClass("invert");
+    $( ".thanks" ).append( "<span>" + text + "</span>" );
+    event.preventDefault();
+  
+    
+  });
